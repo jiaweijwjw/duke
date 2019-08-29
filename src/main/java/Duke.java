7cb920adc7475d1f44e1d0fc4e29d4b4437a27f3
jiawei <jiaweilim97@gmail.com>
2019-08-30 01:00:57 +0800

    level2 completed. adding to list(array) and listing each element

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,41 @@
 import java.util.*;
 
 public class Duke {
-    // this line means this is the enty point to the program.
+    // this line means this is the entry point to the program.
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
+        // greeting.
         String line = "   ____________________________________________________________\n";
         String greeting = "   Hello! I'm Duke\n" + "   What can I do for you?\n";
         System.out.println(line);
         System.out.println(greeting);
         System.out.println(line);
-        // read in input until there is a "bye".
+
+        // reading in input from user.
         Scanner sc = new Scanner(System.in);
+
+        // Obj is the type of object to be stored in List. (type-safe list)
+        // List<Obj> list = new ArrayList<Obj> ();
+        List<String> myList = new ArrayList<String>();
+
+        // keep reading input till there is "bye".
         while(true) {
-            String command = sc.nextLine();
-            if (command.equals("bye")) {
+            String command = sc.nextLine(); // store input into variable called command.
+            if (command.equals("list")) {
                 System.out.println(line);
-                System.out.println("   " + command);
+                // use for loop because want to print the number. if not can use enhanced for loop or iterator.
+                for (int i = 0; i < myList.size(); i++) {
+                    System.out.println("    " + (i + 1) + ". " + myList.get(i));
+                }
                 System.out.println(line);
-                break;
+            } else if (command.equals("bye")) {
+                System.out.println(line);
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break; // to get out of the while(true) loop.
             } else {
+                myList.add(command);
                 System.out.println(line);
-                System.out.println("   " + command);
+                System.out.println("    added: " + command);
                 System.out.println(line);
             }
         }
